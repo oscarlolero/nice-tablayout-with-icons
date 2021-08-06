677f83d56010aacdc9ebffda87549576e335b677
Oscar Montes <oscmcojc1@live.com.mx>
2021-08-05 19:55:23 -0500

    Migrated to ViewPager2, final implementation of tabLayout

--- app/src/main/java/com/jonajo/testingviewpager/MainActivity.java
+++ app/src/main/java/com/jonajo/testingviewpager/MainActivity.java
@@ -1,33 +1,65 @@
 package com.jonajo.testingviewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = findViewById(R.id.tabLayout);
-        viewPager = findViewById(R.id.viewpager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager2);
 
-        tabLayout.setupWithViewPager(viewPager);
-        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        vpAdapter.addFragment(new frag1());
-        vpAdapter.addFragment(new frag2());
-        vpAdapter.addFragment(new frag3());
+        ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+        fragmentArrayList.add(new frag1());
+        fragmentArrayList.add(new frag2());
+        fragmentArrayList.add(new frag3());
 
-        viewPager.setAdapter(vpAdapter);
+        VPAdapter vpAdapter = new VPAdapter(this, fragmentArrayList);
+        viewPager2.setAdapter(vpAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if(position == 0) {
+                    tab.setIcon(R.drawable.ic_selected);
+                } else {
+                    tab.setIcon(R.drawable.ic_not_selected);
+                }
+            }
+        }).attach();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.setIcon(R.drawable.ic_selected);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.setIcon(R.drawable.ic_not_selected);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                tab.setIcon(R.drawable.ic_selected);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/jonajo/testingviewpager/VPAdapter.java
+++ app/src/main/java/com/jonajo/testingviewpager/VPAdapter.java
@@ -3,32 +3,27 @@ package com.jonajo.testingviewpager;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 
-public class VPAdapter extends FragmentPagerAdapter {
+public class VPAdapter extends FragmentStateAdapter {
 
-    private final ArrayList<Fragment> fragmentArrayList =  new ArrayList<>();
+    private final ArrayList<Fragment> fragmentArrayList;
 
-    public VPAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public VPAdapter(@NonNull FragmentActivity fragmentActivity, ArrayList<Fragment> fragmentArrayList) {
+        super(fragmentActivity);
+        this.fragmentArrayList = fragmentArrayList;
     }
 
     @NonNull
     @Override
-    public Fragment getItem(int position) {
+    public Fragment createFragment(int position) {
         return fragmentArrayList.get(position);
     }
 
     @Override
-    public int getCount() {
+    public int getItemCount() {
         return fragmentArrayList.size();
-    }
-
-    public void addFragment(Fragment fragment) {
-        fragmentArrayList.add(fragment);
     }
 }
