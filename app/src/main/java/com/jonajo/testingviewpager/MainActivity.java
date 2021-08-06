17df26ceb7070e17cd07bee1aa2882729e23e8ab
Oscar Montes <oscmcojc1@live.com.mx>
2021-08-05 19:11:38 -0500

    Working with depracated

---
+++ app/src/main/java/com/jonajo/testingviewpager/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jonajo.testingviewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new frag1());
+        vpAdapter.addFragment(new frag2());
+        vpAdapter.addFragment(new frag3());
+
+        viewPager.setAdapter(vpAdapter);
+    }
+}
---
+++ app/src/main/java/com/jonajo/testingviewpager/VPAdapter.java
@@ -0,0 +1,34 @@
+package com.jonajo.testingviewpager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragmentArrayList =  new ArrayList<>();
+
+    public VPAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentArrayList.add(fragment);
+    }
+}
